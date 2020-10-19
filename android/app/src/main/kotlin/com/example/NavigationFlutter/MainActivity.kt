ca533b6cfa07083f5d53d1b7d92eb2e2b00d07ae
Rasheed Qureshi <rasheed.qureshi@cooperativecomputing.com>
2020-10-19 21:20:45 +0500

    project setup

---
+++ android/app/src/main/kotlin/com/example/NavigationFlutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.NavigationFlutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Navigations"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Home Page",
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
