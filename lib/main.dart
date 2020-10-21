3481a1e22bb19548f37796d5fa8d383df23bfc35
MrQureshi <rasheed.qureshi@cooperativecomputing.com>
2020-10-22 02:04:06 +0500

    use of JSON data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'firstScreen.dart';
 import 'secondScreen.dart';
@@ -31,76 +32,88 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Navigations"),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Home Page",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontFamily: "Time New Roman",
-                  fontWeight: FontWeight.w600,
-                )),
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: FlatButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/first');
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //     builder: (context) => FirstScreen(),
-                  //   ),
-                  // );
-                },
-                padding: EdgeInsets.symmetric(
-                  horizontal: 25.0,
-                  vertical: 10.0,
-                ),
-                color: Colors.red,
-                textColor: Colors.white,
-                child: Text(
-                  "First Screen",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10.0),
-              child: FlatButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/second');
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //     builder: (context) => FirstScreen(),
-                  //   ),
-                  // );
-                },
-                padding: EdgeInsets.symmetric(
-                  horizontal: 25.0,
-                  vertical: 10.0,
-                ),
-                color: Colors.red,
-                textColor: Colors.white,
-                child: Text(
-                  "Second Screen",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+      backgroundColor: Colors.grey,
+      body: FutureBuilder(
+        future: DefaultAssetBundle.of(context).loadString('asset/data.json'),
+        builder: (context, snapshot) {
+          var myData = json.decode(snapshot.data.toString());
+          return Center(
+            child: Text(myData['salary'], style: TextStyle(fontSize: 40.0)),
+          );
+        },
       ),
     );
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text("Navigations"),
+    //   ),
+    //   body: Container(
+    //     alignment: Alignment.center,
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: <Widget>[
+    //         Text("Home Page",
+    //             textAlign: TextAlign.center,
+    //             style: TextStyle(
+    //               fontSize: 40.0,
+    //               fontFamily: "Time New Roman",
+    //               fontWeight: FontWeight.w600,
+    //             )),
+    //         Padding(
+    //           padding: EdgeInsets.all(10.0),
+    //           child: FlatButton(
+    //             onPressed: () {
+    //               Navigator.pushNamed(context, '/first');
+    //               // Navigator.push(
+    //               //   context,
+    //               //   MaterialPageRoute(
+    //               //     builder: (context) => FirstScreen(),
+    //               //   ),
+    //               // );
+    //             },
+    //             padding: EdgeInsets.symmetric(
+    //               horizontal: 25.0,
+    //               vertical: 10.0,
+    //             ),
+    //             color: Colors.red,
+    //             textColor: Colors.white,
+    //             child: Text(
+    //               "First Screen",
+    //               style: TextStyle(
+    //                 fontSize: 20.0,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //         Padding(
+    //           padding: EdgeInsets.all(10.0),
+    //           child: FlatButton(
+    //             onPressed: () {
+    //               Navigator.pushNamed(context, '/second');
+    //               // Navigator.push(
+    //               //   context,
+    //               //   MaterialPageRoute(
+    //               //     builder: (context) => FirstScreen(),
+    //               //   ),
+    //               // );
+    //             },
+    //             padding: EdgeInsets.symmetric(
+    //               horizontal: 25.0,
+    //               vertical: 10.0,
+    //             ),
+    //             color: Colors.red,
+    //             textColor: Colors.white,
+    //             child: Text(
+    //               "Second Screen",
+    //               style: TextStyle(
+    //                 fontSize: 20.0,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
